363d8eddbeb6f094a68c6b5b4109454c2207e74b
Billy Everyteen <nikolasapayne@gmail.com>
2016-03-08 23:11:39 -0600

    adding color changes on click

--- js/app.js
+++ js/app.js
@@ -432,4 +432,18 @@ $(document).ready(function () {
         var y = parseInt(e.pageY - $(this).offset().top) - (ripple.height() / 2);
         ripple.css({top: y, left: x}).addClass('animate');
     });
+    var body = document.getElementsByTagName("body");
+    body[0].addEventListener('click', function() {
+      if (body[0].classList.contains("adapt")) {
+        body[0].classList.remove("adapt");
+        body[0].classList.add("parra");
+      } else if(body[0].classList.contains("parra")) {
+        body[0].classList.remove("parra");
+        body[0].classList.add("spotify");
+      } else if(body[0].classList.contains("spotify")) {
+        body[0].classList.remove("spotify");
+        body[0].classList.add("adapt");
+      }
+    }, false);
+
 });
