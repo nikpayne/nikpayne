19809c1480a08666cd9536d18e21aae1683c1123
Billy Everyteen <nikolasapayne@gmail.com>
2016-03-09 17:29:15 -0600

    linking up app.js

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function enqueue_scripts(){
-  wp_enqueue_style( 'style.css', get_stylesheet_uri() );
+  wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_script( 'app', get_template_directory_uri() . 'js/app.js', array( 'jquery' ),  false );
 }
 add_action('wp_enqueue_scripts', 'enqueue_scripts');
 ?>
