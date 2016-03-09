08ab747d352d3b96d44efed4d57f399bca5d9d92
Billy Everyteen <nikolasapayne@gmail.com>
2016-03-09 17:38:29 -0600

    testing app.js

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function enqueue_scripts(){
   wp_enqueue_style( 'style', get_stylesheet_uri() );
-  wp_enqueue_script( 'app', get_template_directory_uri() . '/js/app.js', array( 'jquery' ),  false );
+  wp_enqueue_script( 'app', get_template_directory_uri() . '/js/app.js', array( 'jquery' ),  true );
 }
 add_action('wp_enqueue_scripts', 'enqueue_scripts');
 ?>
