9636721a5e17c58cf09b225e6550ab68836f558c
Billy Everyteen <nikolasapayne@gmail.com>
2016-03-09 17:11:11 -0600

    modifying functions.php

--- functions.php
+++ functions.php
@@ -1,3 +1,3 @@
 <?php
-wp_enqueue_style( "style.css", "./style.css", array(), true, "all" );
+wp_enqueue_style( 'style.css', get_template_directory_uri() . './style.css', array(), true, 'all' );
 ?>
